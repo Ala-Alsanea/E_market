9ab87af93e37f06bf09dc8649e7de364727b986e
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2022-11-16 15:39:56 +0300

    Done Splash

--- lib/Config/components/splashScreen.dart
+++ lib/Config/components/splashScreen.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:e_maecket/Config/style.dart';
 import 'package:e_maecket/main.dart';
 import 'package:e_maecket/pages/browse_page.dart';
+import 'package:e_maecket/pages/layout_page.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class SpalshScreen extends StatelessWidget {
       splash: Center(
         child: Image.asset('assets/img/png/logo.png'),
       ),
-      nextScreen: MyApp(),
+      nextScreen: LayoutPage(),
       splashIconSize: 300,
       duration: 1500,
       splashTransition: SplashTransition.fadeTransition,
